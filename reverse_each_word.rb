f208d02a572b73b66c5c9bb6fe17b63aecd9c7cd
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-14 03:57:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,14 +4,14 @@ def reverse_each_word(array)
   return reversed_array.join(" ")
 end
 
-# FIRST REFACTORED CODE
+
 def reverse_each_word(array)
   new_array = array.split(" ")
   new_array.collect {|x| x.reverse!}
   new_array.join(" ")
 end
 
-# FINAL SOLUTION
+
 def reverse_each_word(array)
   array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
