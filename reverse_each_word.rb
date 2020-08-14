ac5c5a9baa0f7e9a63af61e11153a83d1ac0fce7
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-14 03:56:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
-def reverse_each_word(sentence1)
-  puts sentence1.split.map {|word| word.reverse}.join(" ")
-end 
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
 
-def reverse_each_word(sentence2)
-  
-  
-end 
+# FIRST REFACTORED CODE
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
 
+# FINAL SOLUTION
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
